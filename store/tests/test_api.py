8fb4ea3e2b67b23dfdb900c2bf7ae7a6030bb123
Алексей Чупин <a.chupin@dm-solutions.ru>
2020-12-05 23:05:47 +0300

    API tests create, update and delete added

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,5 @@
+import json
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,6 +10,7 @@ from store.serializers import BookSerializer
 
 class BookApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Clean coder', price=500.00, author='author_1')
         self.book_2 = Book.objects.create(name='Clean code', price=600.00, author='author_2')
         self.book_3 = Book.objects.create(name='Code complete author_1', price=1000.00, author='author_3')
@@ -25,3 +28,39 @@ class BookApiTestCase(APITestCase):
         serializer_data = BookSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.count())
+        url = reverse('book-list')
+        data = {
+            "name": "Ulysses",
+            "price": "350.00",
+            "author": "James Augustine Aloysius Joyce"
+        }
+        self.client.force_login(self.user)
+        json_data = json.dumps(data)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": "400.00",
+            "author": self.book_1.author
+        }
+        self.client.force_login(self.user)
+        json_data = json.dumps(data)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(400, self.book_1.price)
+
+    def test_delete(self):
+        self.assertEqual(3, Book.objects.count())
+        url = reverse('book-detail', args=(self.book_1.id,))
+        self.client.force_login(self.user)
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Book.objects.count())
--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -6,25 +6,28 @@ from store.serializers import BookSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Clean coder', price=500.00)
-        book_2 = Book.objects.create(name='Clean code', price=600.00)
-        book_3 = Book.objects.create(name='Code complete', price=1000.00)
+        book_1 = Book.objects.create(name='Clean coder', price=500.00, author='Robert C. Martin')
+        book_2 = Book.objects.create(name='Clean code', price=600.00, author='Robert C. Martin')
+        book_3 = Book.objects.create(name='Code complete', price=1000.00, author='Steve McConnell')
         data = BookSerializer([book_1, book_2, book_3], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Clean coder',
-                'price': '500.00'
+                'price': '500.00',
+                'author': 'Robert C. Martin',
             },
             {
                 'id': book_2.id,
                 'name': 'Clean code',
-                'price': '600.00'
+                'price': '600.00',
+                'author': 'Robert C. Martin'
             },
             {
                 'id': book_3.id,
                 'name': 'Code complete',
-                'price': '1000.00'
+                'price': '1000.00',
+                'author': 'Steve McConnell'
             },
         ]
         self.assertEqual(expected_data, data)
